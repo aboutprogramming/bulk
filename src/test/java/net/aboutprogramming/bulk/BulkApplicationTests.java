277161432634aaffcba1c5e5483f81010f2f3a14
AP <aboutprogramming@protonmail.com>
2020-08-19 21:40:28 +0800

    init spring boot project

---
+++ src/test/java/net/aboutprogramming/bulk/BulkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aboutprogramming.bulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
